d0375ea61d3a8e6b6389c5a78dce4350038e5917
somozadev <somozadev@gmail.com>
2021-09-20 18:57:49 +0200

    first cmake proyect

---
+++ build/CMakeFiles/Proyecto_Test.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Proyecto_Test.
+# This may be replaced when dependencies are built.
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/mesom/CARRERA_2021/5_carrera
+# Build directory: C:/Users/mesom/CARRERA_2021/5_carrera/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
