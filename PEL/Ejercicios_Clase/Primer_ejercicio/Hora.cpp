f26abac1541ba7f53171b61233c297a1f9fb1a8c
somozadev <somozadev@gmail.com>
2021-09-28 01:54:16 +0200

    clase 28/9/21

--- PEL/Ejercicios_Clase/Primer_ejercicio/Hora.cpp
+++ PEL/Ejercicios_Clase/Primer_ejercicio/Hora.cpp
@@ -6,7 +6,7 @@ class Hora
 public:
     int h, m, s;
     void Visualize();
-    Hora Add(Hora obj1, Hora obj2);
+    void Add(Hora obj1, Hora obj2);
     void Set(Hora hora);
 
     Hora() { this->h = this->m = this->s = 0; }
@@ -23,24 +23,22 @@ void Hora::Visualize()
     cout << this->h << ":" << this->m << ":" << this->s;
 }
 
-Hora Hora::Add(Hora obj1, Hora obj2)
+void Hora::Add(Hora obj1, Hora obj2)
 {
-    Hora horaObj;
-
-    horaObj.h = obj2.h + obj1.h;
-    horaObj.m = obj2.m + obj1.m;
-    horaObj.s = obj2.s + obj1.s;
-    return(horaObj);
-}
-
-
+    this->s = obj2.s + obj1.s;
+    this->m = obj2.m + obj1.m + this->s / 60;
+    this->s = this->s % 60;
+    this->h = obj2.h + obj1.h + this->m/60;
+    this->m = this->m % 60;
+};
 
 int main()
 {
-    Hora hora1(1,2,3);
-    Hora hora2(2,4,6);
+    Hora hora1(12, 44, 3);
+    Hora hora2(12, 1, 6);
 
-    Hora horaVacia = hora1.Add(hora1,hora2);
+    Hora horaVacia;
+    horaVacia.Add(hora1, hora2);
     horaVacia.Visualize();
     return 0;
 }
