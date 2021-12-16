23af61a7da00d473a1148947ebc2a3567048930f
somozadev <somozadev@gmail.com>
2021-12-16 11:07:38 +0100

    grupal assignment

---
+++ PEL/Grupo3_AC3/Practica_grupal_ej1/build/CMakeFiles/Ejercicio1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ejercicio1.dir/main.cpp.obj"
+  "CMakeFiles/Ejercicio1.dir/main.cpp.obj.d"
+  "Ejercicio1.exe"
+  "Ejercicio1.exe.manifest"
+  "Ejercicio1.pdb"
+  "libEjercicio1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ejercicio1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
